f0c7ecb9b69d78a528b2da534cfb9334e19a74df
caesium137bit <asstkm212038@gmail.com>
2021-05-09 11:30:31 +0900

    500エラー

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,10 +1,13 @@
+import './bootstrap'
 import Vue from 'vue'
 import router from './router'
+import store from './store' // ★　追加
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   router,
+  store, // ★　追加
   components: { App },
   template: '<App />'
 })
